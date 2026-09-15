29c2f5dae70e82f9927fae0fe87ae1a42cbd8131
Drzet <zet@doczet.net>
2026-09-15 22:30:14 +0100

    Add display pin configuration

---
+++ main/app_config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "driver/spi_master.h"
+
+#define ICG_LCD_HOST       SPI2_HOST
+#define ICG_LCD_PIN_CS     28
+#define ICG_LCD_PIN_RST    17
+#define ICG_LCD_PIN_DC     16
+#define ICG_LCD_PIN_MOSI   29
+#define ICG_LCD_PIN_SCLK   30
+#define ICG_LCD_PIN_MISO   31
+#define ICG_LCD_WIDTH      480
+#define ICG_LCD_HEIGHT     320
+#define ICG_PREVIEW_HEIGHT 280
