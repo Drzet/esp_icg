6954e4dc3cdd47259a8c78ea1ebc5e0b9cc64c38
Drzet <zet@doczet.net>
2026-09-15 22:56:32 +0100

    Raise LCD SPI clock to 60 MHz

--- main/display.c
+++ main/display.c
@@ -74,7 +74,7 @@ esp_err_t display_init(void)
     esp_lcd_panel_io_spi_config_t io_cfg = {
         .dc_gpio_num = ICG_LCD_PIN_DC,
         .cs_gpio_num = ICG_LCD_PIN_CS,
-        .pclk_hz = 40 * 1000 * 1000,
+        .pclk_hz = 60 * 1000 * 1000,
         .lcd_cmd_bits = 8,
         .lcd_param_bits = 8,
         .spi_mode = 0,
