b5c04d5b2c8188d87aa90c1b282bbdee6abbd148
Drzet <zet@doczet.net>
2026-09-16 21:50:19 +0100

    Add XPT2046 touch driver interface

---
+++ main/touch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "esp_err.h"
+
+typedef struct {
+    int x;
+    int y;
+    uint16_t raw_x;
+    uint16_t raw_y;
+} touch_point_t;
+
+esp_err_t touch_init(void);
+bool touch_read(touch_point_t *point);
